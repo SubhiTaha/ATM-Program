b4a0710e6e524cb4d46b53250aec8ef8d075cfae
Soubhi <soubhitaha1313@gmail.com>
2023-01-27 17:57:31 +0000

    Initial Commit

---
+++ Tutorial2.2/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Tutorial2.2
+//
+//  Created by Mohamad Subhi Taha on 27/01/2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
